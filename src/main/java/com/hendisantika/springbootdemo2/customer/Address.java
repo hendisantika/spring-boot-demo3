0b1c8c114723ee4b366fabdf96173cfd90a4e491
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:53:12 +0700

    #23 - Add Address Entity ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootdemo2/customer/Address.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springbootdemo2.customer;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.envers.Audited;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/26/23
+ * Time: 09:52
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Audited
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String streetAddress;
+    private @NonNull String city;
+    private @NonNull String stateCode;
+    private @NonNull String country;
+    private @NonNull String zipCode;
+
+    @ManyToOne
+    @JsonBackReference
+    private Customer customer;\
+}
