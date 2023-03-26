f366d292a19038fbc360e99cca15a79f3f6564db
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:51:27 +0700

    #21 - Add getAuthenticationManager in SecurityConfig ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,5 +56,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return authenticationManager();
     }
 }
