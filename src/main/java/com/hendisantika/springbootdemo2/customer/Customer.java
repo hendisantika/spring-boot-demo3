ce3d0c6f72523418c4ecc6b5cac0a83d45aac9bc
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:53:59 +0700

    #24 - Add Customer Entity ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootdemo2/customer/Customer.java
@@ -0,0 +1,78 @@
+package com.hendisantika.springbootdemo2.customer;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/26/23
+ * Time: 09:53
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited(withModifiedFlag = true)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private @NonNull String firstName;
+    private @NonNull String lastName;
+    private @NonNull String emailAddress;
+
+    @NotAudited
+    @OneToMany(fetch = FetchType.EAGER, orphanRemoval = true, targetEntity = Address.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    @JsonManagedReference
+    private List<Address> addresses;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    private String updatedBy;
+
+    @Column(name = "created_on")
+    @CreatedDate
+    private Date createdOn;
+
+    @Column(name = "updated_on")
+    @LastModifiedDate
+    private Date updatedOn;
+
+    public void setAddresses(List<Address> addresses) {
+        if(this.addresses == null) {
+            this.addresses = new ArrayList<>();
+        }else {
+            this.addresses.clear();
+        }
+        this.addresses.addAll(addresses);
+    }
+}
