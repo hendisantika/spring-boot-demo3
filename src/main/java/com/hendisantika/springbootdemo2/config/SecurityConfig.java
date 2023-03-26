59e68e9cbdc71e24409332c7c63cb351e23a6fbb
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:51:15 +0700

    #20 - Add getPasswordEncoder in SecurityConfig ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.hendisantika.springbootdemo2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -42,5 +46,14 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
                 .anyRequest().authenticated();
         return http.build();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
