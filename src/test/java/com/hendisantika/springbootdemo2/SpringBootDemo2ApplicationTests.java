035a90e46bc22e329e92208b2f581a19c8706bbe
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:25:02 +0700

    #1 - Initial Commit ✏️📗:octocat:

---
+++ src/test/java/com/hendisantika/springbootdemo2/SpringBootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
