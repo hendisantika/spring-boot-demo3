8c38afd7ea7d46cf7b47a86e459d41b2973eeab4
hendisantika <hendisantika@yahoo.co.id>
2023-03-26 09:50:29 +0700

    #19 - Add configure in SecurityConfig ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootdemo2/config/SecurityConfig.java
@@ -2,6 +2,11 @@ package com.hendisantika.springbootdemo2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,24 @@ public class SecurityConfig {
 
     @Autowired
     private UserService userService;
+
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(getJWTAuthTokenFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/api/v1/auth/token").permitAll()
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                .anyRequest().authenticated();
+        return http.build();
+    }
 }
