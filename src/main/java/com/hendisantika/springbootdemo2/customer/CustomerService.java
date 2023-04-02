3845950dc112ad3f69b533245f601fd8fc319210
hendisantika <hendisantika@yahoo.co.id>
2023-04-02 08:15:01 +0700

    #27 - Add getCustomerById in CustomerService ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/customer/CustomerService.java
+++ src/main/java/com/hendisantika/springbootdemo2/customer/CustomerService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.envers.AuditReader;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +29,9 @@ public class CustomerService {
 
     private final AuditReader auditReader;
 
+    @Cacheable
+    public Optional<Customer> getCustomerById(Long customerId) {
+        log.info("Fetching customer by id: {}", customerId);
+        return customerRepository.findById(customerId);
+    }
 }
