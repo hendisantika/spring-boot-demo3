a742c0ebf45b615c6ce70d8d55b94004483cb1d5
hendisantika <hendisantika@yahoo.co.id>
2023-04-02 08:19:02 +0700

    #40 - Add updateCustomer in CustomerController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/customer/CustomerController.java
+++ src/main/java/com/hendisantika/springbootdemo2/customer/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -77,6 +78,17 @@ public class CustomerController {
         try {
             Customer createdCustomer = customerService.create(customer);
             return ResponseEntity.created(new URI("/customer/" + createdCustomer.getId())).body(customer);
+        }catch(Exception ex) {
+            return handleException(ex);
+        }
+    }
+
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateCustomer(@PathVariable(name = "id") String customerId,  @RequestBody Customer customer) {
+        try {
+            customer.setId(Long.valueOf(customerId));
+            Customer updatedCustomer = customerService.update(customer);
+            return ResponseEntity.ok(updatedCustomer);
         }catch(Exception ex) {
             return handleException(ex);
         }
