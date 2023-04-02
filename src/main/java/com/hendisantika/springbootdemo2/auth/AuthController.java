e6d973f66e951b56f2f054492c5854c766b5f308
hendisantika <hendisantika@yahoo.co.id>
2023-04-02 08:21:15 +0700

    #45 - Add getToken in AuthController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/auth/AuthController.java
+++ src/main/java/com/hendisantika/springbootdemo2/auth/AuthController.java
@@ -1,7 +1,15 @@
 package com.hendisantika.springbootdemo2.auth;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +31,26 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     private final JWTUtil jwtUtil;
+
+    @PostMapping(path = "/token",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getToken(@Valid @RequestBody LoginRequest loginRequest) {
+
+        UsernamePasswordAuthenticationToken loginCredentials =
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUserName(), loginRequest.getPassword());
+
+        Authentication authentication =
+                authenticationManager.authenticate(loginCredentials);
+
+        User user = (User) authentication.getPrincipal();
+        String jwtToken = jwtUtil.createJWT(user);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .build();
+
+    }
 }
