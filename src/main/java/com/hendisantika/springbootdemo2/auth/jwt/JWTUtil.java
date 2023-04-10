3cf0fda04b4669e5950b6cb6e2ce46c8fed27c1a
hendisantika <hendisantika@yahoo.co.id>
2023-04-11 05:23:57 +0700

    #49 - Add createJWT in JWTUtil ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootdemo2/auth/jwt/JWTUtil.java
+++ src/main/java/com/hendisantika/springbootdemo2/auth/jwt/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootdemo2.auth.jwt;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
@@ -7,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -44,5 +47,20 @@ public class JWTUtil {
     @PostConstruct
     public void setUpSecretKey() {
         secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createJWT(User user) {
+        String jwt =
+                Jwts.builder()
+                        .setId(UUID.randomUUID().toString())
+                        .setSubject(user.getUsername())
+                        .setIssuer(issuer)
+                        .setIssuedAt(Date.from(Instant.now()))
+                        .setExpiration(Date.from(Instant.now().plus(Duration.ofSeconds(timeToLiveInSeconds))))
+                        .claim(CLAIM_FIRST_NAME_KEY, user.getFirstName())
+                        .claim(CLAIM_LAST_NAME_KEY, user.getLastName())
+                        .signWith(secretKey)
+                        .compact();
+        return jwt;
     }
 }
